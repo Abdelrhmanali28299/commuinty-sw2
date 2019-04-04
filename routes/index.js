cbcd0d1d91e967b73dc685755b8a7eaaac53bdb2
Abdelrhman Ali <abdoali28299@gmail.com>
2019-04-04 19:50:25 +0200

    save post in db

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,15 @@ const router = express.Router()
 
 router.post('/add', (req, res) => {
 
-    
+    let post = new Post({
+        description: req.body.body,
+        user: req.user.id
+    })
+    post
+        .save()
+        .then(data => {
+            res.json(data)
+        })
 
 })
 
