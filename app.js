8e14a2c26221494738baa29fb23f41d620f372bb
Abdelrhman Ali <abdoali28299@gmail.com>
2019-04-05 19:28:42 +0200

    correct some things

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express")
 const path = require('path')
 const bodyParser = require('body-parser')
 const post = require('./routes/index')
-app.use(express.static(path.join(__dirname, 'public')))
+
 const app = express()
 
 app.use(bodyParser.urlencoded({ extended: false }))
