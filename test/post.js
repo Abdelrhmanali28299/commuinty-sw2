98a42045350ec272e43093d88084334bdf21653c
abdelrhman <abdo_201333@hotmail.com>
2019-05-02 00:26:41 +0200

    Post endpoints test

--- test/post.js
+++ test/post.js
@@ -2,7 +2,16 @@ const mocha = require("mocha")
 const assert = require("assert")
 
 describe("Post endpoints test", () => {
-    it("get post endpoint", () => {
-        assert(1 === 1)
+    it("add post endpoint", async () => {
+        let body = {
+            "id": "123456",
+            "body": "lolol7",
+            "type": "public"
+        }
+        let posts = await post.addPost(body)
+
+        assert(posts.writerId === "123456")
+
     })
+
 })
