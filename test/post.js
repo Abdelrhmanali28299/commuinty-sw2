eaa33c927c7ceae62466b516a3ce0ee8a1a9c7eb
abdelrhman <abdo_201333@hotmail.com>
2019-05-02 00:29:07 +0200

    GET by id endpoints test

--- test/post.js
+++ test/post.js
@@ -1,5 +1,12 @@
 const mocha = require("mocha")
 const assert = require("assert")
+var should = require('chai').should();
+const app = require("../app")
+const Post = require('../model/post')
+const user = require('../model/user')
+const PostDB = require('../model/postDB')
+const UserDB = require('../model/userDB')
+const post = new Post(PostDB, UserDB)
 
 describe("Post endpoints test", () => {
     it("add post endpoint", async () => {
@@ -21,6 +28,15 @@ describe("GET endpoints test", () => {
         let id = "5cc9c0018f7b173110c3009d"
         let posts = await post.getPost(id)
         assert(posts.id === "5cc9c0018f7b173110c3009d")
+    })
+
+})
+
+describe("GET by id endpoints test", () => {
+    it("get post endpoint", async () => {
+        let id = "5"
+        let posts = await post.getPostsOfUser(id)
+        assert(posts[0].writerId === "5")
     })
 
 })
