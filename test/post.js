0e7f8f92581a9d6fe6293e708d7b04a0f0d4961d
Abdelrhman Ali <abdoali28299@gmail.com>
2019-05-01 16:41:34 +0200

    test add post

--- test/post.js
+++ test/post.js
@@ -1,8 +1,20 @@
 const mocha = require("mocha")
 const assert = require("assert")
+const app = require("../app")
+const Post = require('../model/post')
+const PostDB = require('../model/postDB')
+const UserDB = require('../model/userDB')
+const post = new Post(PostDB, UserDB)
 
 describe("Post endpoints test", () => {
-    it("get post endpoint", () => {
-        assert(1 === 1)
+    it("add post endpoint", async () => {
+        let body = {
+            "id": "123456",
+            "body": "lolol7",
+            "type": "public"
+        }
+        let posts = await post.addPost(body)
+
+        assert(posts.writerId === "123456")
     })
 })
